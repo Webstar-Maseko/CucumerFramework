1c10854ae6db4ef46cc592c4f20eed4703379d86
Webstar <webstarsiyabonga@gmail.com>
2024-05-11 20:15:27 +0200

    remove redundant Base class

--- src/test/java/org/webstar/BaseTest/TestContextSetup.java
+++ src/test/java/org/webstar/BaseTest/TestContextSetup.java
@@ -1,19 +1,40 @@
 package org.webstar.BaseTest;
 
 import org.openqa.selenium.WebDriver;
+import org.webstar.Factory.BrowserConfig.BrowserSetup;
 import org.webstar.Pages.PageObjectManager;
+import org.webstar.Utilities.ReadProperties;
+
+import java.util.Properties;
 
 public class TestContextSetup {
 
     public WebDriver driver;
     public PageObjectManager pageObjectManager;
     public String productName;
-    Base base;
+    BrowserSetup setup;
+    Properties prop;
+
 
     public TestContextSetup(){
-        base = new Base();
-        driver = base.getDriver();;
+        setup = new BrowserSetup();
+        prop = new ReadProperties().init_Prop();
+        driver = getDriver();
         pageObjectManager = new PageObjectManager(driver);
     }
+
+    public WebDriver getDriver(){
+        if(driver == null){
+            String browser = System.getProperty("browser") == null? prop.getProperty("browser"): System.getProperty("browser");
+            setup.browserSetup(browser);
+            driver = setup.getDriver();
+            driver.manage().window().maximize();
+
+            driver.get(prop.getProperty("url"));
+        }
+
+        return driver;
+    }
+
 
 }
